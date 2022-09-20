e41bd3560838eaefc54f4ce747894146364560a7
yyc-git <395976266@qq.com>
2022-09-20 11:02:39 +0800

    feat(3_meta3d_triangle): add extensions, contributes and protocols
    pass run test;

---
+++ extension_protocols/wegbpuraytracingedu-use-engine-protocol/src/service/DependentMapType.ts
@@ -0,0 +1,15 @@
+export type dependentExtensionNameMap = {
+    meta3dBsMostExtensionName: string,
+    meta3dEngineCoreExtensionName: string,
+}
+
+export type dependentContributeNameMap = {
+    meta3dWorkPluginRootContributeName: string,
+    WegbpuraytracingeduWorkPluginWebGPUTriangleContributeName: string,
+    meta3dComponentTransformContributeName: string,
+    meta3dComponentGeometryContributeName: string,
+    meta3dComponentPBRMaterialContributeName: string,
+    meta3dComponentBasicCameraViewContributeName: string,
+    meta3dComponentPerspectiveCameraProjectionContributeName: string,
+    meta3dGameObjectContributeName: string,
+}
